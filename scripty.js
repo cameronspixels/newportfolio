0b0119acee529273161ee7119e3f4f597c9091a2
Cameron Barnes <cameronspixels@gmail.com>
2019-04-01 21:16:17 +0100

    Changes.

--- scripty.js
+++ scripty.js
@@ -15,6 +15,31 @@ window.onload = function startScripts() {
     document.getElementById('header').classList.toggle('nav-open');
   };
 
+  function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        scrollTo(element, to, duration - 10);
+    }, 10);
+}
+
+  document.getElementById('goto').onclick = function(e) {
+    e.preventDefault();
+
+    var destId = this.getAttribute('href');
+
+    var destination = document.getElementById('#content');
+
+    scrollTo(document.body, destination.offsetTop, 600);
+
+  }
+
+
+
   
 
 };
